4e54c3a4d36c06d1d2550f679b7709c9a798e83d
johnterrys <johnterryhfm93@gmail.com>
2021-02-19 18:30:12 -0600

    Created Data folder with PatientContext

---
+++ HealthcareApplications/HealthcareApplications/Data/PatientContext.cs
@@ -0,0 +1,19 @@
+﻿using HealthcareApplications.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthcareApplications.Data
+{
+    public class PatientContext : DbContext
+    {
+        public PatientContext(DbContextOptions<PatientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
