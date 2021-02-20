a09fd10b90523f2c9fd7eaddbfeeb41455b08fe5
johnterrys <johnterryhfm93@gmail.com>
2021-02-19 18:38:00 -0600

    Added dependecy injection

--- HealthcareApplications/HealthcareApplications/Startup.cs
+++ HealthcareApplications/HealthcareApplications/Startup.cs
@@ -8,8 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MvcMovie.Data;
 using Microsoft.EntityFrameworkCore;
+using HealthcareApplications.Data;
 
 namespace HealthcareApplications
 {
@@ -26,6 +26,7 @@ namespace HealthcareApplications
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<PatientContext>(options => options.UseSqlServer(Configuration.GetConnectionString("PatientContext"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
