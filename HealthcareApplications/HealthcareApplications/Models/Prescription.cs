dbc96c49393be1591f1837106593e68405cc2436
johnterrys <johnterryhfm93@gmail.com>
2021-02-21 15:45:20 -0600

    Added Databases for Physician, Patients, and Prescription

--- HealthcareApplications/HealthcareApplications/Models/Prescription.cs
+++ HealthcareApplications/HealthcareApplications/Models/Prescription.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,6 @@ namespace HealthcareApplications.Models
 {
     public class Prescription
     {
+        public int Id { get; set; }
     }
 }
