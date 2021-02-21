33fc1d5c393d01f7fb69b1465ae722ea33b45cfc
johnterrys <johnterryhfm93@gmail.com>
2021-02-20 18:17:20 -0600

    Added Patient Contex to controller

--- HealthcareApplications/HealthcareApplications/Controllers/PhysicianController.cs
+++ HealthcareApplications/HealthcareApplications/Controllers/PhysicianController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HealthcareApplications.Data;
 using HealthcareApplications.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,13 @@ namespace HealthcareApplications.Controllers
 {
     public class PhysicianController : Controller
     {
+        private readonly PatientContext _patientContext;
+
+        public PhysicianController(PatientContext patientContext)
+        {
+            _patientContext = patientContext;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -18,6 +26,7 @@ namespace HealthcareApplications.Controllers
         {
             List<Patient> patients = new List<Patient>();
 
+            //_patientContext.Patients.Select(patient => patient.Id == physicianId);
 
             return patients;
         }
