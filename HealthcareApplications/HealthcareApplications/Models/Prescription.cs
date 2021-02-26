227a274172c853ab0a4117fdce45085d07a04430
johnterrys <johnterryhfm93@gmail.com>
2021-02-25 22:19:16 -0600

    Fleshed out Prescription model

--- HealthcareApplications/HealthcareApplications/Models/Prescription.cs
+++ HealthcareApplications/HealthcareApplications/Models/Prescription.cs
@@ -9,5 +9,14 @@ namespace HealthcareApplications.Models
     public class Prescription
     {
         public int Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Physician PrescribingPhysician { get; set; }
+        public Patient PrescribedPatient { get; set; }
+        public Drug PrescribedDrug { get; set; }
+        public int Quantity { get; set; }
+        public String Dosage { get; set; }
+        public int RefillCount { get; set; }
     }
 }
