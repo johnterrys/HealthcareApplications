21404723ee4e9ff3a6fb3ef282f39cecadd130f1
johnterrys <johnterryhfm93@gmail.com>
2021-03-19 18:42:15 -0500

    Missing reference

--- HealthcareApplications/HealthcareApplications/Data/UserContext.cs
+++ HealthcareApplications/HealthcareApplications/Data/UserContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HealthcareApplications.Models.UserModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
