c8b508e49a0d8e0ed7747fc53d82163307a25a07
johnterrys <johnterryhfm93@gmail.com>
2021-03-19 18:39:32 -0500

    Added UserContext and User model

---
+++ HealthcareApplications/HealthcareApplications/Models/UserModels/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthcareApplications.Models.UserModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Username { get; set; }
+        public String SecQ1Index { get; set; }
+        public String SecQ2Index { get; set; }
+        public String SecQ3Index { get; set; }
+        public int AccountStatus { get; set; }
+    }
+}
