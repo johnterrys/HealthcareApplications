7b98f1bcbefc0ca63a67a40ab52500627ab92174
johnterrys <johnterryhfm93@gmail.com>
2021-03-19 20:40:54 -0500

    Working data inputs

--- HealthcareApplications/HealthcareApplications/Controllers/HomeController.cs
+++ HealthcareApplications/HealthcareApplications/Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HealthcareApplications.Models;
+using HealthcareApplications.Models.UserModels;
+using HealthcareApplications.Data;
 
 namespace HealthcareApplications.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly UserContext _context;
+        private User loggedIn;
+        public HomeController(ILogger<HomeController> logger, UserContext context)
         {
             _logger = logger;
+            _context = context;
+            loggedIn = null;
         }
 
         public IActionResult Index()
@@ -42,6 +47,37 @@ namespace HealthcareApplications.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                var foundUser = _context.Users.First(a => a.Username.Equals(objUser.Username) && a.Password.Equals(a.Password));
+                if (foundUser != null)
+                {
+                    //Session["UserID"] = obj.UserId.ToString();
+                    //Session["UserName"] = obj.UserName.ToString();
+                    loggedIn = foundUser;
+                    return RedirectToAction("UserDashBoard");
+
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (loggedIn != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
     }
 }
--- HealthcareApplications/HealthcareApplications/Models/UserModels/User.cs
+++ HealthcareApplications/HealthcareApplications/Models/UserModels/User.cs
@@ -10,9 +10,9 @@ namespace HealthcareApplications.Models.UserModels
         public int Id { get; set; }
         public String Username { get; set; }
         public String Password { get; set; }
-        public String SecQ1Index { get; set; }
-        public String SecQ2Index { get; set; }
-        public String SecQ3Index { get; set; }
+        public int SecQ1Index { get; set; }
+        public int SecQ2Index { get; set; }
+        public int SecQ3Index { get; set; }
         public int AccountStatus { get; set; }
     }
 }
