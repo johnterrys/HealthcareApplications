95c2bb96cbacb5621013c59a8789234e27ecb644
johnterrys <johnterryhfm93@gmail.com>
2021-04-25 09:33:05 -0500

    Login changes

--- HealthcareApplications/HealthcareApplications/Controllers/HomeController.cs
+++ HealthcareApplications/HealthcareApplications/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ using HealthcareApplications.Data;
 using Microsoft.AspNetCore.Http;
 using System.Security.Cryptography;
 using System.Text;
+using InsuranceApplication.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HealthcareApplications.Controllers
 {
@@ -36,6 +38,8 @@ namespace HealthcareApplications.Controllers
         private const string SecurityQuestionNum = "SecurityQuestionNum";
         private const string SecurityQuestionText = "SecurityQuestionText";
         private const string SecurityQuestionsAttempted = "SecurityQuestionsAttempted";
+        public static string UserId = "UserId";
+        public static string Name = "Name";
 
         public HomeController(ILogger<HomeController> logger, UserContext context, PatientContext patientContext, PhysicianContext physicianContext)
         {
@@ -86,7 +90,9 @@ namespace HealthcareApplications.Controllers
                 {
                     enteredUser.Username = HttpContext.Session.GetString("Username");
                 }
-                var foundUser = _userContext.Users.FirstOrDefault(a => a.Username.Equals(enteredUser.Username));
+
+                User foundUser = _userContext.Users.FirstOrDefault(a => a.Username.Equals(enteredUser.Username));
+
                 if (foundUser == null)
                 {
                     HttpContext.Session.SetString("Username", "");
@@ -99,10 +105,25 @@ namespace HealthcareApplications.Controllers
                     HttpContext.Session.SetString(SecurityQuestionNum, "4");
                     return View();
                 }
+                if (foundUser.Salt == null)
+                {
+                    byte[] newSalt = new byte[32];
+                    random.NextBytes(newSalt);
+                    foundUser.Salt = newSalt;
+                    _userContext.Update(foundUser);
+                    _userContext.SaveChanges();
+                }
+                SHA512 hasher = new SHA512Managed();
                 //No security question responses, so check if password is correct
                 if (enteredUser.SecQ1Response == null && enteredUser.SecQ2Response == null && enteredUser.SecQ3Response == null)
+                //if (enteredUser.SecQ1Response == null && enteredUser.SecQ2Response == null && enteredUser.SecQ3Response == null && !HttpContext.Session.GetString(SecurityQuestionsAttempted).Contains("1"))
                 {
-                    if (foundUser.Password.Equals(enteredUser.Password))
+
+                    byte[] saltedPwd = Encoding.ASCII.GetBytes(enteredUser.Password + Encoding.ASCII.GetString(foundUser.Salt));
+                    byte[] saltedHashedPwd = hasher.ComputeHash(saltedPwd);
+
+                    //if (foundUser.PasswordHash == null || foundUser.PasswordHash.SequenceEqual(saltedHashedPwd))
+                    if (foundUser.PasswordHash.SequenceEqual(saltedHashedPwd))
                     {
                         //send to first security question
                         int nextQuestionNum = random.Next(1, 4);
@@ -129,21 +150,31 @@ namespace HealthcareApplications.Controllers
                     }
                     return View(enteredUser);
                 }
-                //Check if any are right
-                if ((enteredUser.SecQ1Response != null && enteredUser.SecQ1Response.Equals(foundUser.SecQ1Response)) ||
-                   (enteredUser.SecQ2Response != null && enteredUser.SecQ2Response.Equals(foundUser.SecQ2Response)) ||
-                   (enteredUser.SecQ3Response != null && enteredUser.SecQ3Response.Equals(foundUser.SecQ3Response)))
+                byte[] saltedQ1 = Encoding.ASCII.GetBytes(enteredUser.SecQ1Response + Encoding.ASCII.GetString(foundUser.Salt));
+                byte[] saltedHashedQ1 = hasher.ComputeHash(saltedQ1);
+                byte[] saltedQ2 = Encoding.ASCII.GetBytes(enteredUser.SecQ2Response + Encoding.ASCII.GetString(foundUser.Salt));
+                byte[] saltedHashedQ2 = hasher.ComputeHash(saltedQ2);
+                byte[] saltedQ3 = Encoding.ASCII.GetBytes(enteredUser.SecQ3Response + Encoding.ASCII.GetString(foundUser.Salt));
+                byte[] saltedHashedQ3 = hasher.ComputeHash(saltedQ3);
+
+                //if(foundUser.SecQ1ResponseHash == null || foundUser.SecQ2ResponseHash == null || foundUser.SecQ3ResponseHash == null)
+                if ((enteredUser.SecQ1Response != null && saltedHashedQ1.SequenceEqual(foundUser.SecQ1ResponseHash)) ||
+                   (enteredUser.SecQ2Response != null && saltedHashedQ2.SequenceEqual(foundUser.SecQ2ResponseHash)) ||
+                   (enteredUser.SecQ3Response != null && saltedHashedQ3.SequenceEqual(foundUser.SecQ3ResponseHash)))
                 {
                     bool isPatient = _patientContext.Patients.FirstOrDefault(a => a.UserId == foundUser.Id) != null;
                     HttpContext.Session.SetString("Role", isPatient ? "Patient" : "Physician");
+                    HttpContext.Session.SetString(UserId, foundUser.Id.ToString());
                     if (isPatient)
                     {
                         var patient = _patientContext.Patients.FirstOrDefault(a => a.UserId == foundUser.Id);
+                        HttpContext.Session.SetString(Name, patient.Name);
                         HttpContext.Session.SetString("PatientId", patient.Id.ToString());
                     }
                     else
                     {
                         var physician = _physicianContext.Physicians.FirstOrDefault(a => a.UserId == foundUser.Id);
+                        HttpContext.Session.SetString(Name, physician.Name);
                         HttpContext.Session.SetString("PhysicianId", physician.Id.ToString());
                     }
                     //send to user dashboard ;
@@ -230,6 +261,141 @@ namespace HealthcareApplications.Controllers
             }
         }
 
+        public ActionResult MyDetails()
+        {
+            User foundUser = _userContext.Users.First(u => u.Id.ToString() == HttpContext.Session.GetString(UserId));
+            Patient foundPatient = null;
+            Physician foundPhysician = null;
+            if(HttpContext.Session.GetString("Role") == "Patient")
+            {
+                foundPatient = _patientContext.Patients.First(p => p.UserId == foundUser.Id);
+            }
+            else
+            {
+                foundPhysician = _physicianContext.Physicians.First(p => p.UserId == foundUser.Id);
+            }
+            UserDetailsViewModel vm = new UserDetailsViewModel
+            {
+                CurrentUser = foundUser,
+                CurrentPatient = foundPatient,
+                CurrentPhysician = foundPhysician
+            };
+            return View(vm);
+        }
+
+        public ActionResult EditMyDetails()
+        {
+            User foundUser = _userContext.Users.First(u => u.Id.ToString() == HttpContext.Session.GetString(UserId));
+            Patient foundPatient = null;
+            Physician foundPhysician = null;
+            if (HttpContext.Session.GetString("Role") == "Patient")
+            {
+                foundPatient = _patientContext.Patients.First(p => p.UserId == foundUser.Id);
+            }
+            else
+            {
+                foundPhysician = _physicianContext.Physicians.First(p => p.UserId == foundUser.Id);
+            }
+            UserDetailsViewModel vm = new UserDetailsViewModel
+            {
+                CurrentUser = foundUser,
+                CurrentPatient = foundPatient,
+                CurrentPhysician = foundPhysician
+            };
+            vm.Questions = GetSelectListItems(SecurityQuestions);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult EditMyDetails(UserDetailsViewModel vm)
+        {
+            if (!ModelState.IsValid || vm.CurrentUser.SecQ2Index == vm.CurrentUser.SecQ1Index || vm.CurrentUser.SecQ3Index == vm.CurrentUser.SecQ1Index || vm.CurrentUser.SecQ3Index == vm.CurrentUser.SecQ2Index)
+            {
+                vm.Questions = GetSelectListItems(SecurityQuestions);
+                return View(vm);
+            }
+            User foundUser = _userContext.Users.First(u => u.Id.ToString() == HttpContext.Session.GetString(UserId));
+            Patient foundPatient = null;
+            Physician foundPhysician = null;
+            if (HttpContext.Session.GetString("Role") == "Patient")
+            {
+                foundPatient = _patientContext.Patients.First(p => p.UserId == foundUser.Id);
+            }
+            else
+            {
+                foundPhysician = _physicianContext.Physicians.First(p => p.UserId == foundUser.Id);
+            }
+
+
+            SHA512 hasher = new SHA512Managed();
+
+            if (!string.IsNullOrEmpty(vm.CurrentUser.Password))
+            {
+                byte[] saltedPwd = Encoding.ASCII.GetBytes(vm.CurrentUser.Password + Encoding.ASCII.GetString(foundUser.Salt));
+                byte[] saltedHashedPwd = hasher.ComputeHash(saltedPwd);
+                foundUser.PasswordHash = saltedHashedPwd;
+            }
+
+            foundUser.SecQ1Index = vm.CurrentUser.SecQ1Index;
+            foundUser.SecQ2Index = vm.CurrentUser.SecQ2Index;
+            foundUser.SecQ3Index = vm.CurrentUser.SecQ3Index;
+
+            if (!string.IsNullOrEmpty(vm.CurrentUser.SecQ1Response))
+            {
+                byte[] saltedQ1 = Encoding.ASCII.GetBytes(vm.CurrentUser.SecQ1Response + Encoding.ASCII.GetString(foundUser.Salt));
+                byte[] saltedHashedQ1 = hasher.ComputeHash(saltedQ1);
+                foundUser.SecQ1ResponseHash = saltedHashedQ1;
+            }
+            if (!string.IsNullOrEmpty(vm.CurrentUser.SecQ2Response))
+            {
+                byte[] saltedQ2 = Encoding.ASCII.GetBytes(vm.CurrentUser.SecQ2Response + Encoding.ASCII.GetString(foundUser.Salt));
+                byte[] saltedHashedQ2 = hasher.ComputeHash(saltedQ2);
+                foundUser.SecQ2ResponseHash = saltedHashedQ2;
+            }
+            if (!string.IsNullOrEmpty(vm.CurrentUser.SecQ3Response))
+            {
+                byte[] saltedQ3 = Encoding.ASCII.GetBytes(vm.CurrentUser.SecQ3Response + Encoding.ASCII.GetString(foundUser.Salt));
+                byte[] saltedHashedQ3 = hasher.ComputeHash(saltedQ3);
+                foundUser.SecQ3ResponseHash = saltedHashedQ3;
+            }
+
+            if (HttpContext.Session.GetString("Role") == "Patient")
+            {
+                if (!string.IsNullOrEmpty(vm.CurrentPatient.Name))
+                {
+                    foundPatient.Name = vm.CurrentPatient.Name;
+                }
+                if (!string.IsNullOrEmpty(vm.CurrentPatient.Pronouns))
+                {
+                    foundPatient.Pronouns = vm.CurrentPatient.Pronouns;
+                }
+                _patientContext.Patients.Update(foundPatient);
+                _patientContext.SaveChanges();
+                HttpContext.Session.SetString(Name, foundPatient.Name);
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(vm.CurrentPhysician.Name))
+                {
+                    foundPhysician.Name = vm.CurrentPhysician.Name;
+                }
+                if (!string.IsNullOrEmpty(vm.CurrentPhysician.Pronouns))
+                {
+                    foundPhysician.Pronouns = vm.CurrentPhysician.Pronouns;
+                }
+                _physicianContext.Physicians.Update(foundPhysician);
+                _physicianContext.SaveChanges();
+                HttpContext.Session.SetString(Name, foundPhysician.Name);
+            }
+
+            _userContext.Users.Update(foundUser);
+            _userContext.SaveChanges();
+
+            HttpContext.Session.SetString("Username", foundUser.Username);
+
+            return RedirectToAction("MyDetails");
+        }
+
         public ActionResult LogOut()
         {
             HttpContext.Session.SetString("Username", "");
@@ -239,6 +405,21 @@ namespace HealthcareApplications.Controllers
             HttpContext.Session.SetString("PatientId", "");
             HttpContext.Session.SetString("PhysicianId", "");
             return RedirectToAction("Login");
+        }
+
+        private IEnumerable<SelectListItem> GetSelectListItems(IEnumerable<string> elements)
+        {
+            var selectList = new List<SelectListItem>();
+            for (int i = 0; i < elements.Count(); i++)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = i.ToString(),
+                    Text = elements.ElementAt(i)
+                }); ;
+            }
+
+            return selectList;
         }
     }
 }
